PennPanda/liblitmus-1

Add resctrl, a tool for setting up reservations
resctrl is a new tool intended to configure reservations in
reservation-aware plugins. For now, it only supports the partitioned
reservations (P-RES) plugin.

(While at it, also update .gitignore)

Makefile
@@ -73,7 +73,7 @@ AR  := ${CROSS_COMPILE}${AR}
 
 all     = lib ${rt-apps}
 rt-apps = cycles base_task rt_launch rtspin release_ts measure_syscall \
-	  base_mt_task uncache runtests
+	  base_mt_task uncache runtests resctrl
 
 .PHONY: all lib clean dump-config TAGS tags cscope help doc
 
@@ -233,6 +233,9 @@ obj-release_ts = release_ts.o
 
 obj-measure_syscall = null_call.o
 lib-measure_syscall = -lm
+
+obj-resctrl = resctrl.o
+
 
 # ##############################################################################
 # Build everything that depends on liblitmus.

LITMUS-RT/liblitmus

Makefile
@@ -73,7 +73,7 @@ AR  := ${CROSS_COMPILE}${AR}
 
 all     = lib ${rt-apps}
 rt-apps = cycles base_task rt_launch rtspin release_ts measure_syscall \
-	  base_mt_task uncache runtests
+	  base_mt_task uncache runtests resctl
 
 .PHONY: all lib clean dump-config TAGS tags cscope help doc
 
@@ -234,6 +234,9 @@ obj-release_ts = release_ts.o
 
 obj-measure_syscall = null_call.o
 lib-measure_syscall = -lm
+
+obj-resctl = resctl.o
+
 
 # ##############################################################################
 # Build everything that depends on liblitmus.